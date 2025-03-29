7e4530e6fdaff61bbf6e253da1d0536a2732f1ee
ThainaDev96 <rthaina776@gmail.com>
2025-03-28 22:09:59 -0300

    Create auladepython.py
    primeira aula de python

---
+++ auladepython.py
@@ -0,0 +1,18 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):#def define as variáveis 
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+lista = [1,2,3,4,5]
+#range tranforma uma lista em uma sequencia de numeros 
+print (lista)
+for i in range(len(lista)):
+    print(f"posicao[{i}]) : {lista[i]}")
+    #chaves para representa um parametro ou valor do F de função
+print(f"Obrigado por usar nossa aplicação")
+
